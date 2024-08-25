24da8cf1ed49ecf7f827027774fd916322bebb2c
Neno Arisma <nenoarisma@gmail.com>
2024-08-25 10:28:14 +0700

    chore: integrate database to airplanes dashboard

---
+++ app/dashboard/(home)/airplanes/components/columns-table.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Airplane } from "@prisma/client";
+import type { ColumnDef } from "@tanstack/react-table";
+import { Pencil } from "lucide-react";
+import Link from "next/link";
+
+
+export const columns: ColumnDef<Airplane>[] = [
+  {
+    accessorKey: 'image',
+    header: 'Image'
+  },
+  {
+    accessorKey: 'code',
+    header: 'Code',
+  },
+  {
+    accessorKey: 'name',
+    header: 'Name'
+  },
+  {
+    id: 'actions',
+    cell: ({ row }) => {
+      const plane = row.original
+
+      return (
+        <div className="inline-flex gap-5 items-center">
+          <Button variant="secondary" asChild size='sm'>
+            <Link href={`/dashboard/airplanes/edit/${plane.id}`}>
+              <Pencil className='mr-2 h-4 w-4' />
+              Edit Data</Link>
+          </Button>
+        </div>
+      )
+    }
+  }
+]
