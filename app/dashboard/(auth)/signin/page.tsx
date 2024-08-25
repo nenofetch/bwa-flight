ce3ed1fd5d4256bd292c97fb1539f43c92904a84
Neno Arisma <nenoarisma@gmail.com>
2024-08-25 12:15:36 +0700

    chore: remove unused props

--- app/dashboard/(auth)/signin/page.tsx
+++ app/dashboard/(auth)/signin/page.tsx
@@ -1,21 +1,16 @@
 
 import { Metadata } from 'next';
-import React, { type FC } from 'react';
+import React from 'react';
 import FormSignIn from './form';
 import { getUser } from '@/lib/auth';
 import { redirect } from 'next/navigation';
-
-
-interface SignInPageProps {
-
-}
 
 export const metadata: Metadata = {
     title: 'Dashboard | SignIn'
 }
 
 
-export default async function Page(props: FC<SignInPageProps>): Promise<React.ReactElement> {
+export default async function Page(): Promise<React.ReactElement> {
     const { session, user } = await getUser();
 
     if (session && user.role === 'ADMIN') return redirect('/dashboard');
