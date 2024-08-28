29d8bbd31b6c01fd3ce23558d31807ca252e9f3b
Neno Arisma <nenoarisma@gmail.com>
2024-08-28 15:21:34 +0700

    feat: add supabase lib

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,1 +1,27 @@
 import { createClient } from "@supabase/supabase-js"
+
+
+const NEXT_PUBLIC_SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
+const NEXT_PUBLIC_SUPABASE_PUBLIC_KEY = process.env.NEXT_PUBLIC_SUPABASE_PUBLIC_KEY ?? "";
+
+
+const supabase = createClient(
+  NEXT_PUBLIC_SUPABASE_URL,
+  NEXT_PUBLIC_SUPABASE_PUBLIC_KEY
+);
+
+export const uploadFile = async (file: File) => {
+  try {
+    const filename = `${Date.now()}.png`;
+
+
+    const { error } = await supabase.storage
+      .from("avatars")
+      .upload(`public/airplanes/${filename}`, file, {
+        cacheControl: "3600",
+        upsert: false
+      })
+  } catch (error) {
+    throw error;
+  }
+}
